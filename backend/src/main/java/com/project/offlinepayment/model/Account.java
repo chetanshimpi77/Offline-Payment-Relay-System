b2032dd5d2d922bbba38c4d184a3f8afdad33333
mrchetanshimpi <mrchetanshimpi@gmail.com>
2026-06-12 18:54:25 +0530

    account entity and repo created

---
+++ backend/src/main/java/com/project/offlinepayment/model/Account.java
@@ -0,0 +1,27 @@
+package com.project.offlinepayment.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "accounts")
+public class Account {
+    @Id
+    private String vpa;
+
+    @Column(nullable = false)
+    private String holderName;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal balance;
+
+    @Version
+    private Long version;
+}
